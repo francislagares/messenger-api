0eb638d8f6cc98ae66adaf75cb8ab9e95212e634
Francis Lagares <francis.lagares@gmail.com>
2023-01-14 19:37:46 +0100

    feat: api-gateway and auth module

---
+++ apps/api-gateway/src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authService: ClientProxy,
+  ) {}
+
+  @Get()
+  async getUser() {
+    return this.authService.send(
+      {
+        cmd: 'get-user',
+      },
+      {},
+    );
+  }
+}
